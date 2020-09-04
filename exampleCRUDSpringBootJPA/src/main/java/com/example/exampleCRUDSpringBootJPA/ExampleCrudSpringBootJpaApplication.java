b8606bd20aea50901f14ddd9e6e1474aef636b7c
HenriqueCaio <64160089+HenriqueCaio@users.noreply.github.com>
2020-09-04 12:07:46 -0300

    Add files via upload
    First commit

---
+++ exampleCRUDSpringBootJPA/src/main/java/com/example/exampleCRUDSpringBootJPA/ExampleCrudSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleCRUDSpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCrudSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCrudSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ exampleCRUDSpringBootJPA/src/test/java/com/example/exampleCRUDSpringBootJPA/ExampleCrudSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleCRUDSpringBootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleCrudSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
